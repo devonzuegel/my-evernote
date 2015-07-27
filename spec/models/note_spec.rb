dc70f95c77e447f6c8cb66aa59ad38ad76b044ae
John Backus <jbackus@stanford.edu>
2015-07-27 03:08:21 -0700

    Make life a bit easier with shoulda matchers

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
+  it { should validate_presence_of(:guid) }
+  it { should validate_uniqueness_of(:guid) }
+
   describe '.sync' do
     subject(:sync) { -> { Note.sync(attributes) } }
     let(:first_note) { Note.first }
@@ -30,22 +33,6 @@ RSpec.describe Note, type: :model do
         expect(first_note.title).to eq(note.title)
         expect(first_note.title).to_not eq(attributes.fetch(:title))
       end
-    end
-  end
-
-  describe 'Creating a new note' do
-    it 'should increment Note.count by one' do
-      expect { create(:note) }.to change { Note.count }.by 1
-    end
-
-    it 'should require a guid' do
-      expect { Note.create! }.to raise_error ActiveRecord::RecordInvalid
-    end
-
-    it 'should require guids to be unique' do
-      same_guid = Faker::Lorem.characters(20)
-      create(:note, guid: same_guid)
-      expect { create(:note, guid: same_guid) }.to raise_error ActiveRecord::RecordInvalid
     end
   end
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'shoulda/matchers'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -51,4 +52,16 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      # Choose a test framework:
+      with.test_framework :rspec
+
+      # Choose one or more libraries:
+      with.library :active_record
+      with.library :active_model
+      with.library :action_controller
+    end
+  end
 end
